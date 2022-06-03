2f2ddc8fee162fa75f47b355a3d4048cb0f4ee1b
bryan23453 <rayan.andino@gmail.com>
2022-06-03 13:27:00 -0600

    first commit

---
+++ P3Lab7_BryanAlexander/P3Lab7_BryanAlexander.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout << "----------Bienvenido----------\n";
+	int opcion=1;
+	while (opcion!=0){
+		cout << "0. Salir" << endl;
+		cout << "1. Ingresar numero" << endl;
+		cout << "2. Listar numero" << endl;
+		cout << "3. Operaciones" << endl;
+		cout << "Ingrese Opcion: "; cin >> opcion;
+		switch (opcion) {
+		case 0: {
+			cout << "Feliz Dia!";
+		}
+			  break;
+		case 1: {
+
+		}
+			  break;
+		case 2: {
+
+		}
+			  break;
+		case 3: {
+
+		}
+			  break;
+		default: {
+
+		}
+			   break;
+		}
+	}
+	
+}
+
